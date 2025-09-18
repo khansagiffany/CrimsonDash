52c6a4eafa1fa596be2c26f53024af8ec9d3d27d
khansagiffany <khansaagiffany@gmail.com>
2025-09-18 12:16:22 +0700

    package n config

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+// src/app/not-found.tsx
+export default function NotFound() {
+    return (
+      <div className="h-screen flex items-center justify-center bg-black text-white">
+        <h1 className="text-4xl">404 - Page Not Found</h1>
+      </div>
+    )
+  }
+  
