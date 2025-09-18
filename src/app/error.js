629f2bc8f1a4075ea42906cb81b4d9e7a17ce72c
khansagiffany <khansaagiffany@gmail.com>
2025-09-18 12:29:34 +0700

    add error.js

---
+++ src/app/error.js
@@ -0,0 +1,14 @@
+// app/error.js
+'use client'
+ 
+export default function Error({
+  error,
+  reset,
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
